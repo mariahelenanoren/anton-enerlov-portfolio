bd68f18bb1722d934512708be1db08333a6bd367
jenerlov <jenerlov@gmail.com>
2021-12-30 15:15:13 +0100

    added lifestyle-page

---
+++ pages/lifestyle.tsx
@@ -0,0 +1,13 @@
+export default function LifeStylePage() {
+	return (
+		<div>
+			<p>Lifestyle Page</p>
+		</div>
+	);
+}
+
+export async function getStaticProps() {
+	return {
+		props: {},
+	};
+}
