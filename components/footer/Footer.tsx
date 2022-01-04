ae5135d59ded838b109f0d2687a57eca49205586
jenerlov <jenerlov@gmail.com>
2022-01-04 16:11:37 +0100

    added copyright

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,30 +1,39 @@
-import Link from "next/link";
-import { Image } from "react-datocms";
+import { Box, Link } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Image as DatoImage } from "react-datocms";
+import { PaddingComponent } from "..";
 import { useStyles } from "./styles";
 import { IFooter } from "./types";
+import Image from "next/image";
 
-export default function Footer({footer}: IFooter){
-    const classes = useStyles();
-   
-    console.log(footer)
-	return(
-        <div className={classes.footer}>
-            {footer.socialLinks.map((link, index) => 
-            <>
+export default function Footer({ footer }: IFooter) {
+	const classes = useStyles();
+	const [date, setDate] = useState(Number);
+	const getYear = () => setDate(new Date().getFullYear());
 
-          
-            <Image 
-                data={link.icon.responsiveImage}
-                className={classes.socialIcons}
-                key={index}>{link.link} 
-            </Image> 
-          
+	useEffect(() => {
+		getYear();
+	}, []);
 
-       
-        </>       
-      )};
-            </div>
-        )};
-      
-            {/* <p key={index}>{link.link}</p>   */}
-         
+	return (
+		<PaddingComponent>
+			<div className={classes.footer}>
+				<div className={classes.logo}>
+					<Image
+						layout="fill"
+						src="/assets/logo-cropped.png"
+						alt="logotype"
+					></Image>
+				</div>
+				{footer.socialLinks.map((link, index) => (
+					<Link key={index} href={link.link}>
+						<div className={classes.socialIcons}>
+							<DatoImage data={link.icon.responsiveImage}></DatoImage>
+						</div>
+					</Link>
+				))}
+				;<span className={classes.copyName}>&copy; Anton Enerlöv {date}</span>
+			</div>
+		</PaddingComponent>
+	);
+}
