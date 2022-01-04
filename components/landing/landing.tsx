a2b7623bad917b74a5d9ff24ea10d6d4c4625f7f
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-04 15:56:59 +0100

    fix comments

--- components/landing/landing.tsx
+++ components/landing/landing.tsx
@@ -48,38 +48,4 @@ export default function Landing({ landing }: ILanding) {
       </div>
     </div>
   );
-  //   return (
-  //     <div className={classes.landingContainer}>
-  //       <div className={classes.landingInner}>
-  //         <div className={classes.imageContainer}>
-  //           <Image
-  //             data={landing.backgroundImage.responsiveImage}
-  //             className={classes.image}
-  //           />
-  //         </div>
-  //         <div className={classes.overlay}></div>
-  //         <Grid container spacing={2} className={classes.contentContainer}>
-  //           <Grid item>
-  //             <Typography variant="h1" textAlign={'center'}>
-  //               {landing.title}
-  //             </Typography>
-  //           </Grid>
-  //           <Grid item>
-  //             <Typography variant="body1" textAlign={'center'}>
-  //               {landing.subtitle}
-  //             </Typography>
-  //           </Grid>
-  //           <Grid item className={classes.iconContainer}>
-  //             {landing.socialLinks.map((link, index) => (
-  //               <Link href={link.link} key={index} className={classes.link}>
-  //                 <Image
-  //                   data={link.icon.responsiveImage}
-  //                   className={classes.icon}
-  //                 />
-  //               </Link>
-  //             ))}
-  //           </Grid>
-  //         </Grid>
-  //       </div>
-  //     </div>
 }
