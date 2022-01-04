db632f9eaa28295c0c3769eb48a6dd3d53ab30d9
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-04 09:53:23 +0100

    styled main fullheight

--- layout/layout/layout.tsx
+++ layout/layout/layout.tsx
@@ -1,11 +1,13 @@
 import React, { PropsWithChildren } from 'react';
 import { Header } from '../../components';
+import { useStyles } from './styles';
 
 export default function Layout({ children }: PropsWithChildren<{}>) {
+  const classes = useStyles();
   return (
     <>
       <Header />
-      <div>{children}</div>
+      <main className={classes.main}>{children}</main>
     </>
   );
 }
