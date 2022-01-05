a5d09ce058b02e35f2a684063b335958bc48b4a7
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-05 12:12:44 +0100

    added image overlay on hover

--- components/grid/grid.tsx
+++ components/grid/grid.tsx
@@ -1,17 +1,29 @@
 import { Masonry } from '@mui/lab';
-import { Paper } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 import { Image } from 'react-datocms';
 import { IProjects } from '../../lib/gql';
 import { PaddingComponent } from '../paddingComponent';
+import { useStyles } from './styles';
 
 export default function Grid({ allProjects }: IProjects) {
+  const classes = useStyles();
+
   return (
     <PaddingComponent>
-      <Masonry columns={2} spacing={1}>
-        {allProjects.map((project, index) => (
-          <Image key={index} data={project.featuredImage.responsiveImage} />
-        ))}
-      </Masonry>
+      <div className={classes.mansonryContainer}>
+        <Masonry columns={2} spacing={2}>
+          {allProjects.map((project, index) => (
+            <div key={index} className={classes.imageContainer}>
+              <Image data={project.featuredImage.responsiveImage} />
+              <div className={classes.overlay}>
+                <Typography className={classes.text} variant="h4">
+                  {project.title}
+                </Typography>
+              </div>
+            </div>
+          ))}
+        </Masonry>
+      </div>
     </PaddingComponent>
   );
 }
