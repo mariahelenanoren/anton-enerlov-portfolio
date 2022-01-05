21bb8c22b9af2bccbe144e8a6080f01afb05612c
jenerlov <jenerlov@gmail.com>
2022-01-05 14:13:07 +0100

    fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,19 +3,11 @@ import { IContact } from "../components/contact/types";
 import { getContactData } from "../lib/gql/contact";
 import { getFooterData } from '../lib/gql';
 
-
-
 export default function ContactPage({contactPage}: IContact) {
 
   return (
-    <>
-      <Contact contactPage={contactPage}/>
-     
-    </>
+      <Contact contactPage={contactPage}/>  
   )
- 
-  
-
 }
 
 export async function getStaticProps() {
