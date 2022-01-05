82d287199d4953787a06f7024e62559d92fefd27
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-05 11:49:49 +0100

    added grid to lifestyle page

--- pages/lifestyle.tsx
+++ pages/lifestyle.tsx
@@ -1,18 +1,27 @@
 import { Landing } from '../components';
-import { getLandingData } from '../lib/gql';
+import Grid from '../components/grid/grid';
+import { getLandingData, getProjectsData, IProjects } from '../lib/gql';
 import { ILanding } from '../lib/gql/landing';
 
-export default function LifestylePage({ landing }: ILanding) {
+interface ILifestylePage extends ILanding, IProjects {}
+
+export default function LifestylePage({
+  landing,
+  allProjects,
+}: ILifestylePage) {
   return (
     <>
       <Landing landing={landing} />
+      <Grid allProjects={allProjects} />
     </>
   );
 }
 
 export async function getStaticProps() {
   const { landing } = await getLandingData();
+  const { allProjects } = await getProjectsData('Lifestyle');
+
   return {
-    props: { landing },
+    props: { landing, allProjects },
   };
 }
