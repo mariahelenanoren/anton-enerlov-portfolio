4a06348c9a1d37b8077b185dc7a3d471f306e765
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-06 13:40:49 +0100

    added custom 404 page

---
+++ layout/404/styles.ts
@@ -0,0 +1,12 @@
+import { Theme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  sectionContainer: {
+    marginTop: '8rem',
+    textAlign: 'center',
+  },
+  button: {
+    marginTop: '2rem',
+  },
+}));
