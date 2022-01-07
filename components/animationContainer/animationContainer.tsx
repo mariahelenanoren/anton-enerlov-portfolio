96b47b9587f6f7dc5e631d9ce6f389e98d91bbfe
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-07 14:27:19 +0100

    removed unused code

--- components/animationContainer/animationContainer.tsx
+++ components/animationContainer/animationContainer.tsx
@@ -1,12 +1,10 @@
 import React, { PropsWithChildren, useEffect, useRef } from 'react';
 import { motion, useAnimation } from 'framer-motion';
-import { useStyles } from './styles';
 import { OnScreen } from '../../lib/helpers/onScreen';
 
 export default function AnimationContainer({
   children,
 }: PropsWithChildren<{}>) {
-  const classes = useStyles();
   const controls = useAnimation();
   const rootRef = useRef<HTMLDivElement | null>(null);
   const onScreen = OnScreen(rootRef);
@@ -26,7 +24,6 @@ export default function AnimationContainer({
 
   return (
     <motion.div
-      className={classes.animationContainer}
       initial={{ opacity: 0, y: 20 }}
       animate={controls}
       ref={rootRef}
