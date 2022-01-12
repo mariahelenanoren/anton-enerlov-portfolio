72eca0d2b5b73eadc2467b3c5ea97775dab46a3c
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-12 11:42:52 +0100

    styled info container

--- components/projectPage/styles.ts
+++ components/projectPage/styles.ts
@@ -2,27 +2,27 @@ import { Theme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles((theme: Theme) => ({
-	projectContainer: {
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		flexDirection: 'column',
-	},
+  projectContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
 
-	titleContainer: {
-		padding: '8rem',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		flexDirection: 'column',
-	},
+  titleContainer: {
+    padding: '8rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
 
-	category: {
-		fontSize: '12px',
-		textTransform: 'uppercase',
-	},
+  category: {
+    fontSize: '12px',
+    textTransform: 'uppercase',
+  },
 
-	firstImage: {},
-
-	imageGrid: {},
+  firstImage: {
+    width: '100%',
+  },
 }));
