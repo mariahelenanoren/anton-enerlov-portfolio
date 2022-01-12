e5d3c628e6df95ad45bb344a65514016f6f8f1a8
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-12 14:19:43 +0100

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,24 +5,54 @@ import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../styles/theme/theme';
 import { Loader } from '../components';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	const [loading, SetLoading] = useState(false);
-	const router = useRouter();
+  const [loading, SetLoading] = useState(false);
+  const router = useRouter();
 
-	useEffect(() => {
-		router.events.on('routeChangeStart', (url) => {
-			SetLoading(true);
-		});
+  useEffect(() => {
+    router.events.on('routeChangeStart', (url) => {
+      SetLoading(true);
+    });
 
-		router.events.on('routeChangeComplete', (url) => {
-			SetLoading(false);
-		});
-	}, [router.events]);
+    router.events.on('routeChangeComplete', (url) => {
+      SetLoading(false);
+    });
+  }, [router.events]);
 
-	return (
-		<ThemeProvider theme={theme}>
-			{loading ? <Loader /> : <Component {...pageProps} />}
-		</ThemeProvider>
-	);
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          color="#5bbad5"
+        ></link>
+        <meta name="msapplication-TileColor" content="#2b5797"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <ThemeProvider theme={theme}>
+        {loading ? <Loader /> : <Component {...pageProps} />}
+      </ThemeProvider>
+    </>
+  );
 }
