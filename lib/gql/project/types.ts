1cb974372d289243021f60f84e719893609c2365
jenerlov <jenerlov@gmail.com>
2022-01-20 08:55:36 +0100

    added product to project

--- lib/gql/project/types.ts
+++ lib/gql/project/types.ts
@@ -1,31 +1,34 @@
 import { ResponsiveImageType } from 'react-datocms';
 
 export interface IProject {
-  id: string;
-  title: string;
-  categoryTitle: string;
-  featuredImage: {
-    responsiveImage: ResponsiveImageType;
-  };
-  images: {
-    responsiveImage: ResponsiveImageType;
-  }[];
-  client: {
-    name?: string;
-  }[];
-  productionCompany: {
-    name?: string;
-  }[];
-  model: {
-    name?: string;
-  }[];
-  producer: {
-    name?: string;
-  }[];
-  agency: {
-    name?: string;
-  }[];
-  retouch: {
-    name?: string;
-  }[];
+	id: string;
+	title: string;
+	categoryTitle: string;
+	featuredImage: {
+		responsiveImage: ResponsiveImageType;
+	};
+	images: {
+		responsiveImage: ResponsiveImageType;
+	}[];
+	client: {
+		name?: string;
+	}[];
+	product: {
+		name?: string;
+	}[];
+	productionCompany: {
+		name?: string;
+	}[];
+	model: {
+		name?: string;
+	}[];
+	producer: {
+		name?: string;
+	}[];
+	agency: {
+		name?: string;
+	}[];
+	retouch: {
+		name?: string;
+	}[];
 }
