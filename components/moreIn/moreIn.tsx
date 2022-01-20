7f880a95db1625053f3cf027a67679160627bfbe
Maria Helena Norén <marialinder97@hotmail.com>
2022-01-20 12:27:05 +0100

    fixed link scroll

--- components/moreIn/moreIn.tsx
+++ components/moreIn/moreIn.tsx
@@ -1,6 +1,5 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Link } from '@mui/material';
 import classNames from 'classnames';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { Image } from 'react-datocms';
 import { PaddingComponent } from '..';
@@ -36,11 +35,7 @@ export default function MoreIn({ allProjects, id }: IMoreIn) {
             >{`More in ${suggestedProjects[0].categoryTitle.toLowerCase()}`}</Typography>
             <Grid container direction="row" spacing={3}>
               {suggestedProjects.map((project, index) => (
-                <Link
-                  key={index}
-                  href={`/${transformToRoute(project.title)}`}
-                  passHref
-                >
+                <Link key={index} href={`/${transformToRoute(project.title)}`}>
                   <Grid item xs={12} md={4}>
                     <div className={classes.itemContent}>
                       <Image
