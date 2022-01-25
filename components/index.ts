9f341fdbe7286122a15e3ac17edbcb12cc89fcd0
jenerlov <jenerlov@gmail.com>
2022-01-25 17:00:17 +0100

    moved components to layout

--- components/index.ts
+++ components/index.ts
@@ -1,33 +1,27 @@
 import { PaddingComponent } from './paddingComponent';
 import { TextContainer } from './textContainer';
 import { Header } from './header';
-import { Landing } from './landing';
 import { Footer } from './footer';
-import { Grid } from './grid';
+import { Grid } from '../layout/grid';
 import { SelectedClients } from './selectedClients';
 import { ProjectTextContainer } from './projectTextContainer';
 import { Loader } from './loader';
 import { ProjectInfo } from './projectInfo';
-import { ProjectPage } from './projectPage';
 import { MediaGrid } from './mediaGrid';
 import { AnimationContainer } from './animationContainer';
-import { Page } from './page';
 import { ExternalVideo } from './externalVideo';
 
 export {
-  PaddingComponent,
-  Header,
-  Footer,
-  Landing,
-  Grid,
-  TextContainer,
-  SelectedClients,
-  ProjectTextContainer,
-  Loader,
-  ProjectInfo,
-  ProjectPage,
-  MediaGrid,
-  AnimationContainer,
-  Page,
-  ExternalVideo,
+	PaddingComponent,
+	Header,
+	Footer,
+	Grid,
+	TextContainer,
+	SelectedClients,
+	ProjectTextContainer,
+	Loader,
+	ProjectInfo,
+	MediaGrid,
+	AnimationContainer,
+	ExternalVideo,
 };
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,4 @@
-import Contact from '../components/contact/contact';
+import Contact from '../layout/contact/contact';
 import { getContactData } from '../lib/gql/contact';
 import { getFooterData } from '../lib/gql';
 import { IContact } from '../lib/gql/contact/types';
@@ -9,23 +9,23 @@ import { Page } from '../components';
 interface IContactPage extends IFooter, IContact {}
 
 export default function ContactPage({ contactPage, footer }: IContactPage) {
-  return (
-    <Page
-      title="Contact"
-      description="Feel free to contact me with any questions or business inquiries."
-    >
-      <Layout footer={footer}>
-        <Contact contactPage={contactPage} />
-      </Layout>
-    </Page>
-  );
+	return (
+		<Page
+			title="Contact"
+			description="Feel free to contact me with any questions or business inquiries."
+		>
+			<Layout footer={footer}>
+				<Contact contactPage={contactPage} />
+			</Layout>
+		</Page>
+	);
 }
 
 export async function getStaticProps() {
-  const { contactPage } = await getContactData();
-  const { footer } = await getFooterData();
+	const { contactPage } = await getContactData();
+	const { footer } = await getFooterData();
 
-  return {
-    props: { contactPage, footer },
-  };
+	return {
+		props: { contactPage, footer },
+	};
 }
